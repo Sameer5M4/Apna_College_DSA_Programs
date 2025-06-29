a8d960dcb731761587d55bad363ab2c2dcc7a42a
Shaik Kabeer Ahamed <239x5a05m4@gprec.ac.in>
2025-06-29 23:38:02 +0530

    add indianCoin proble

--- IndianCoins.py
+++ IndianCoins.py
@@ -1,8 +1,13 @@
 coins=[1,2,5,10,20,50,100,500,2000]
-v=int(intput("enter v value"))
-i,j=0,len(coins)-1
-while i<j :
-  if(coins[j]<v):
-    sum=coins[j]
-    j+=1
-  if coint[i
+v=int(input("enter v value :"))
+coins=coins[::-1]
+count=0
+for x in coins:
+        while x<=v:
+            count+=1
+            v-=x
+print(count)
+
+
+
+    
